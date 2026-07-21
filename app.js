b9b4f605d1c57871d3354df2070ddada57c37303
mishasro72 <mishasro72@gmail.com>
2026-07-21 19:40:02 -0400

    Remove timeController and timeRoutes

--- app.js
+++ app.js
@@ -16,21 +16,6 @@ app.use(express.json());
 app.use("/api", timeRouter);
 app.use("/api/users", userRouter);
 
-app.get("/", (req, res) => {
-  res.send("Hello, World!");
-});
-
-app.post("/testpost", (req, res) => {
-  res.status(200).json({
-    message: "POST route works",
-  });
-});
-
-// app.all("/{*splat}", (req, res) => {
-//   res.status(404).json({
-//     message: `No route found for ${req.method} ${req.path}`,
-//   });
-// });
 app.use(notFound);
 app.use(errorHandler);
 
