fbbfa5b87110a4f86346f0fe3ad48d4142c5dc45
mishasro72 <mishasro72@gmail.com>
2026-07-21 14:55:52 -0400

    Add register, logon, logoff controllers

---
+++ controllers/userController.js
@@ -0,0 +1,32 @@
+function register(req, res) {
+  const { name, email, pasword } = req.body;
+  const user = {
+    id: global.users.length + 1,
+    name: name,
+    email: email,
+    pasword: pasword,
+  };
+  global.users.push(user);
+  global.user_id = user;
+  return res.status(201).json({ name: user.name, email: "user.email" });
+}
+
+function logon(req, res) {
+  const { email, password } = req.body;
+  const matchingUser = global.users.find((user) => user.email === email);
+  if (matchingUser && matchingUser.password === password) {
+    global.user_id = matchingUser;
+    return res
+      .status(200)
+      .json({ name: matchingUser.name, email: matchingUser.email });
+  } else {
+    return res.status(401);
+  }
+}
+
+function logoff(req, res) {
+  global.user_id = null;
+  return res.status(200);
+}
+
+module.exports = { register, logon, logoff };
