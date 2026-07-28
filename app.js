a92f5288dfa3c73aaffd1db8d5ddfe54cf4b887b
mishasro72 <mishasro72@gmail.com>
2026-07-28 17:23:01 -0400

    Add assignment4a

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ const express = require("express");
 const app = express();
 
 const userRouter = require("./routes/userRoutes");
+const taskRouter = require("./routes/taskRoutes");
 
 const errorHandler = require("./middleware/error-handler");
 const notFound = require("./middleware/not-found");
+const authMiddleware = require("./middleware/auth")
 
 global.user_id = null;
 global.users = [];
@@ -13,6 +15,7 @@ global.tasks = [];
 app.use(express.json());
 
 app.use("/api/users", userRouter);
+app.use("/api/tasks", authMiddleware, taskRouter)
 app.use(notFound);
 app.use(errorHandler);
 
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,26 +1,58 @@
-function register(req, res) {
-  const { name, email, password } = req.body;
+const { userSchema } = require("../validation/userSchema");
+const crypto = require("crypto");
+const util = require("util");
+const scrypt = util.promisify(crypto.scrypt);
+
+async function hashPassword(password) {
+  const salt = crypto.randomBytes(16).toString("hex");
+  const derivedKey = await scrypt(password, salt, 64);
+  return `${salt}:${derivedKey.toString("hex")}`;
+}
+
+async function comparePassword(inputPassword, storedHash) {
+  const [salt, key] = storedHash.split(":");
+  const keyBuffer = Buffer.from(key, "hex");
+  const derivedKey = await scrypt(inputPassword, salt, 64);
+  return crypto.timingSafeEqual(keyBuffer, derivedKey);
+}
+
+async function register(req, res) {
+  const { error, value } = userSchema.validate(req.body ?? {}, {
+    abortEarly: false,
+  });
+  if (error) {
+    return res.status(400).json({ message: error.message });
+  }
+
+  const hashedPassword = await hashPassword(value.password);
+
   const user = {
     id: global.users.length + 1,
-    name: name,
-    email: email,
-    password: password,
+    name: value.name,
+    email: value.email,
+    hashedPassword,
   };
   global.users.push(user);
   global.user_id = user;
   return res.status(201).json({ name: user.name, email: user.email });
 }
 
-function logon(req, res) {
+async function logon(req, res) {
   const { email, password } = req.body;
-  const matchingUser = global.users.find((user) => user.email === email && user.password === password);
-  if (!matchingUser) {
+  const matchingUser = global.users.find((user) => user.email === email);
+
+  const goodCredentials =
+    matchingUser &&
+    (await comparePassword(password, matchingUser.hashedPassword));
+  
+    if (!goodCredentials) {
     return res.status(401).json({});
   }
+
   global.user_id = matchingUser;
-    return res
-      .status(200)
-      .json({ name: matchingUser.name, email: matchingUser.email });
+  return res
+    .status(200)
+    .json({ name: matchingUser.name, email: matchingUser.email });
 }
 
 function logoff(req, res) {
