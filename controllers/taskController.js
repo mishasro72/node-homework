052abf4e6623a6e47dfdb15949a1156f40115307
mishasro72 <mishasro72@gmail.com>
2026-08-11 14:53:50 -0400

    Upgrade show: migrate to prisma

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -41,22 +41,30 @@ async function index(req, res) {
   return res.status(200).json(tasks);
 }
 
-async function show(req, res) {
-  const taskId = parseInt(req.params?.id);
-  if (!taskId) {
-    return res.status(400).json({});
+async function show(req, res, next) {
+  const taskId = parseInt(req.params?.id, 10);
+  if (isNaN(taskId)) {
+    return res.status(400).json({ message: "Invalid task ID" });
   }
 
-  const tasks = await pool.query(
-    "SELECT id, title, is_completed FROM tasks WHERE id = $1 AND user_id = $2",
-    [taskId, global.user_id],
-  );
+  try {
+    const task = await prisma.task.findUnique({
+      where: { id: taskId, userId: global.user_id },
+      select: { title: true, isCompleted: true, id: true },
+    });
 
-  if (tasks.rows.length === 0) {
-    return res.status(404).json({});
+    if (!task) {
+      return res.status(404).json({ message: "The task was not found." });
+    }
+
+    return res.status(200).json(task);
+  } catch (err) {
+    if (err.code === "P2025") {
+      return res.status(404).json({ message: "The task was not found." });
+    } else {
+      return next(err);
+    }
   }
-
-  return res.status(200).json(tasks.rows[0]);
 }
 
 async function update(req, res, next) {
@@ -71,7 +79,7 @@ async function update(req, res, next) {
     return res.status(400).json({ message: error.message });
   }
 
-  const id = parseInt(req.params.id, 10);
+  const id = parseInt(req.params?.id, 10);
   try {
     const updatedTask = await prisma.task.update({
       data: taskChange,
